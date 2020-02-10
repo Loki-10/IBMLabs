35400a5205082117b223b24cf448c428c1100535
Lokeswaran <51825122+Loki-10@users.noreply.github.com>
2020-02-09 23:12:36 -0800

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,14 @@
+a=int(input("Enter First Number:"))
+b=int(input("Enter Second Number:"))
+print("Enter Method: 1.Add 2. Subtract 3. Multiply 4. Divide")
+m=int(input())
+if m==1:
+    print(a+b)
+elif m==2:
+    print(a-b)
+elif m==3:
+    print(a*b)
+elif m==4:
+    print(a/b)
+
+
