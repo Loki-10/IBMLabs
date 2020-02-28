7fba0f16d76cce80e54f5460405b23a6ee52e01d
Lokeswaran <51825122+Loki-10@users.noreply.github.com>
2020-02-28 16:18:30 +0530

    Create matrix_multiplication.py

---
+++ matrix_multiplication.py
@@ -0,0 +1,25 @@
+row1 = int(input("Rows of First Matrix:"))
+col1 = int(input("Column of First Matrix:"))
+row2 = int(input("Rows of Second Matrix:"))
+col2 = int(input("Column of Second Matrix:"))
+A = []
+B = []
+for i in range(row1):
+    a = []
+    for j in range(col1):
+        a.append(int(input()))
+    A.append(a)
+for i in range(row2):
+    a = []
+    for j in range(col2):
+        a.append(int(input()))
+    B.append(a)
+result = [[0, 0, 0, 0],
+          [0, 0, 0, 0],
+          [0, 0, 0, 0]]
+for i in range(len(A)):
+    for j in range(len(B[0])):
+        for k in range(len(B)):
+            result[i][j] += A[i][k] * B[k][j]
+for r in result:
+    print(r)
